44d95d344c4a583d408a10392b1d59d20d6e173c
Breta Ionut <breta.ionut@gmail.com>
2017-12-06 01:06:45 +0200

    Refactored the doctrine EM factory

--- src/App/Doctrine/EntityManagerFactory.php
+++ src/App/Doctrine/EntityManagerFactory.php
@@ -2,8 +2,10 @@
 
 namespace App\Doctrine;
 
+use Doctrine\Common\Persistence\Mapping\Driver\SymfonyFileLocator;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Driver\YamlDriver;
 use Doctrine\ORM\Tools\Setup;
 
 /**
@@ -11,6 +13,10 @@ use Doctrine\ORM\Tools\Setup;
  */
 class EntityManagerFactory
 {
+    private const MAPPING_BUNDLE_PATH = 'Resources/config/doctrine';
+    private const MAPPING_BUNDLE_NAMESPACE = 'Model';
+    private const MAPPING_FILES_EXTENSION = 'yaml';
+
     /**
      * Creates an entity manager given the database connection and some application details.
      *
@@ -21,6 +27,7 @@ class EntityManagerFactory
      * @param string $password
      * @param string $database
      * @param string $env      The application environment (dev, prod).
+     * @param array  $bundles  The application bundles metadata.
      *
      * @return EntityManagerInterface
      */
@@ -31,7 +38,8 @@ class EntityManagerFactory
         string $user,
         string $password,
         string $database,
-        string $env
+        string $env,
+        array $bundles
     ): EntityManagerInterface {
         $connParams = [
             'driver' => $driver,
@@ -43,7 +51,21 @@ class EntityManagerFactory
         ];
 
         $isDev = in_array($env, ['dev', 'test'], true);
+
+        // Determine the paths where the mapping files should be located.
+        $mappingPaths = [];
+        foreach ($bundles as $bundle) {
+            $bundleMappingPath = $bundle['path'].'/'.self::MAPPING_BUNDLE_PATH;
+            $bundleMappingNamespace = $bundle['namespace'].'\\'.self::MAPPING_BUNDLE_NAMESPACE;
+
+            $mappingPaths[$bundleMappingPath] = $bundleMappingNamespace;
+        }
+
         $config = Setup::createConfiguration($isDev);
+        $config->setMetadataDriverImpl(new YamlDriver(new SymfonyFileLocator(
+            $mappingPaths,
+            self::MAPPING_FILES_EXTENSION
+        )));
 
         return EntityManager::create($connParams, $config);
     }
