52a17f2d98c885d03223382348838258a670e5bf
Breta Ionut <breta.ionut@gmail.com>
2018-01-13 23:35:04 +0200

    Use debug component

--- public/index.php
+++ public/index.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
-
-error_reporting(-1);
-ini_set('display_errors', 'on');
 
 require __DIR__.'/../vendor/autoload.php';
 
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool) ($_SERVER['APP_DEBUG'] ?? true));
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? true);
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
