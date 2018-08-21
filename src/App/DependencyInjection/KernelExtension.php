e0c8fb1421a4863c286d865822bcd8f4b0a231a6
Ionut Breta <ionut.breta@zitec.com>
2018-08-22 00:21:39 +0300

    PHP

--- src/App/DependencyInjection/KernelExtension.php
+++ src/App/DependencyInjection/KernelExtension.php
@@ -2,10 +2,8 @@
 
 namespace App\DependencyInjection;
 
-use App\DependencyInjection\Compiler\ContainerAwarePass;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -23,9 +21,6 @@ class KernelExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('kernel.yaml');
 
-        // Tag all services implementing the container aware interface with the container aware tag.
-        $container->registerForAutoconfiguration(ContainerAwareInterface::class)
-            ->addTag(ContainerAwarePass::CONTAINER_AWARE_TAG);
         // Register all commands as services.
         $container->registerForAutoconfiguration(Command::class)
             ->addTag('console.command');
